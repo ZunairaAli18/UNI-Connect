9bd57a9b1edeff226895981c1400779daf46895e
ZunairaAli18 <zunaira.ali2053@gmail.com>
2025-01-26 20:54:17 +0500

    hehe

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-
+const cors = require("cors");
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer);
+app.use(cors());
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin:"http://localhost:3000"
+  },
+});
 io.on("connection", (socket) => {
   console.log(socket.id);
   // ...
 });
 
-httpServer.listen(4000, ()=> {
+server.listen(4000, ()=> {
     console.log("4000");
 });
